3faa06a095248ece6b1d3c0c1f436547386cb8a7
Agustin Sampayo <agustines82@gmail.com>
2022-08-14 23:54:00 -0300

    ejercicio 3 listo

--- js/app.js
+++ js/app.js
@@ -1,52 +1,33 @@
-const botonTarea = document.querySelector("#formdescripcion");
-let listado = document.querySelector("ul");
+// menejadores de eventos:
+let inputTarea = document.getElementById("tarea");
+let btnAgregar = document.getElementById("agregar");
+let listado = document.getElementById("listado");
 
-//manejador de eventos:
-botonTarea.addEventListener("submit", agregarTarea);
+btnAgregar.addEventListener("click", agregarTarea);
 
-//funcion:
-function agregarTarea(e) {
-    e.preventDefault();
-    let tareaAgregada = document.querySelector("input").value;
-    console.log(tareaAgregada);
-
-    listado.innerHTML = listado + "<li>" + tareaAgregada + "</li>";
-
-    document.querySelector("#formdescripcion").reset();
-}
-
-/*
-let items = document.getElementById("lista");
-let lista = [];
-
-function agregar() {
-    let val = document.getElementById("lista").value;
-    if (val === "") {
-        alert("Por favor ingrese la tarea primero");
-    } else {
-        lista.push(val.trim());
-        lista.value = "";
-        mostrarLista();
+function agregarTarea() {
+    if (inputTarea.value === "") {
+        return;
     }
-}
+    //tomamos el valor del input:
+    let elemento = inputTarea.value;
+    //creamos el elemento html (li)
+    let li = document.createElement("li");
+    li.innerHTML = elemento;
+    li.className = "lead d-flex align-items-center justify-content-between border-bottom border-secondary ms-2 me-5";
+    //agrego el li al listado
+    listado.appendChild(li);
 
-function mostrarLista() {
-    let datos = "";
-    if (lista.length > 0) {
-        for (let i = 0; i < lista.length; i++) {
-            datos +=
-                "<li class='list-group-item'><button class='pull-right' onclick='removeList(" +
-                i +
-                ")'><span class='glyphicon glyphicon-trash'></span></button>" +
-                lists[i] +
-                "</li>";
-        }
-    }
-    document.getElementById("resultados").innerHTML = datos;
-}
+    //agregamos el boton eliminar:
+    let btnEliminar = document.createElement("span");
+    btnEliminar.innerHTML = `<i class="bi bi-trash text-danger"></i>`;
+    li.appendChild(btnEliminar);
 
-function remover(lista) {
-    lista.splice(lista, 1);
-    mostrarLista();
+    //agregamos la funcionabilidad que elimina:
+    btnEliminar.onclick = function () {
+        li.remove();
+    };
+
+    //vaciamos el input:
+    inputTarea.value = "";
 }
-*/
